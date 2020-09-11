a1f87fdbd0c04ed07de7fa85f75e1dffba2e0313
Paul Christian Rueda <nexuslifeline@gmail.com>
2020-09-11 22:33:04 +0800

    added wave bg on header. improves maintenance message

--- src/views/pages/MyProfile.vue
+++ src/views/pages/MyProfile.vue
@@ -1,41 +1,12 @@
 <template>
-  <div class="my-profile__main-container">
-    <div class="my-profile__announcement-container">
-      <div>Thank you for being patient. We are still doing some work on this feature.</div>
-      <div>We will release an update as soon as we can.</div>
-      <p class="mt-4">- Nexus Team</p>
-    </div>
-  </div>
+  <Maintenance />
 </template>
 
 <script>
+import Maintenance from '../components/Maintenance';
 export default {
-
+  components: {
+    Maintenance
+  }
 }
 </script>
-
-<style lang="scss">
-  @import "../../assets/scss/shared.scss";
-
-  .my-profile__main-container {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    width: 100%;
-    height: 100%;
-    padding: 60px;
-
-    @include for-size(phone-only) {
-      padding: 10px;
-    }
-  }
-
-  .my-profile__announcement-container {
-    height: auto;
-    width: 100%;
-    padding: 40px;
-    font-size: 18px;
-  }
-
-</style>
