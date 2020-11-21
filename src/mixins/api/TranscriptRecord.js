3a7071a3fe5ca9e2e0d361da6858faa4937a0ff9
Jason Patawaran <patawaranjason17@gmail.com>
2020-11-21 18:12:04 +0800

    merge fixes

--- src/mixins/api/TranscriptRecord.js
+++ src/mixins/api/TranscriptRecord.js
@@ -7,6 +7,9 @@ export default {
     },
     getTranscriptRecord(id) {
       return this.$authHttp.get(`${apiPrefix}/${apiVersion}/transcript-records/${id}`);
+  	},
+    getSubjectsOfTranscriptRecord(transcriptRecordId, params) {
+      return this.$authHttp.get(`${apiPrefix}/${apiVersion}/transcript-records/${transcriptRecordId}/subjects`, { params: params });
     },
   }
 }
