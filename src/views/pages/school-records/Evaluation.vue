70af632c54df677b83bc5f0a2cb502b439217437
Jason Patawaran <patawaranjason17@gmail.com>
2020-11-23 23:36:13 +0800

    student evaluation and enrollments

--- src/views/pages/school-records/Evaluation.vue
+++ src/views/pages/school-records/Evaluation.vue
@@ -3,28 +3,31 @@
     <b-card v-if="!showEntry">
       <h5 slot="header">
         <span>
-          Student Evaluations <br>
+          Student Evaluations <br />
           <small>List of all evaluation of the student.</small>
         </span>
       </h5>
       <b-card-body>
         <b-table
           details-td-class="table-secondary"
-          hover outlined small show-empty responsive
+          hover
+          outlined
+          small
+          show-empty
+          responsive
           :fields="tables.evaluations.fields"
           :items.sync="tables.evaluations.items"
-          :busy="tables.evaluations.isBusy">
+          :busy="tables.evaluations.isBusy"
+        >
           <template v-slot:table-busy>
             <div class="text-center my-2">
-              <v-icon
-                name="spinner"
-                spin
-                class="mr-2" />
+              <v-icon name="spinner" spin class="mr-2" />
               <strong>Loading...</strong>
             </div>
           </template>
           <template v-slot:cell(attachments)="data">
-            <span>{{ data.item.filesCount }} &nbsp; &nbsp; &nbsp; &nbsp; </span>  <v-icon name="paperclip"/>
+            <span>{{ data.item.filesCount }} &nbsp; &nbsp; &nbsp; &nbsp; </span>
+            <v-icon name="paperclip" />
           </template>
           <template v-slot:cell(name)="data">
             <b-media>
@@ -33,51 +36,94 @@
                   rounded
                   blank
                   size="64"
-                  :text="data.item.student.firstName.charAt(0) + '' + data.item.student.lastName.charAt(0)"
-                  :src="avatar(data.item.student)" />
+                  :text="
+                    data.item.student.firstName.charAt(0) +
+                      '' +
+                      data.item.student.lastName.charAt(0)
+                  "
+                  :src="avatar(data.item.student)"
+                />
               </template>
-              <span>{{ data.item.student.name }}</span><br>
-              <small>Student no.: {{ data.item.student.studentNo ? data.item.student.studentNo : 'Awaiting Confirmation' }}</small><br>
-              <small>Address : {{ data.item.student.address ?
-                data.item.student.address.currentCompleteAddress : "" }}
+              <span>{{ data.item.student.name }}</span
+              ><br />
+              <small
+                >Student no.:
+                {{
+                  data.item.student.studentNo
+                    ? data.item.student.studentNo
+                    : 'Awaiting Confirmation'
+                }}</small
+              ><br />
+              <small
+                >Address :
+                {{
+                  data.item.student.address
+                    ? data.item.student.address.currentCompleteAddress
+                    : ''
+                }}
               </small>
             </b-media>
           </template>
           <template v-slot:cell(contact)="data">
-            Email : {{ data.item.student.email }} <br>
-            <small>Phone : {{ data.item.student.phoneNo }}</small> <br>
-            <small>Mobile : {{ data.item.student.mobileNo }}</small> <br>
+            Email : {{ data.item.student.email }} <br />
+            <small>Phone : {{ data.item.student.phoneNo }}</small> <br />
+            <small>Mobile : {{ data.item.student.mobileNo }}</small> <br />
           </template>
           <template v-slot:cell(education)="data">
             <span>
-              {{ getName(data.item, 'level') }}<br>
-              {{ getName(data.item, 'course') }}<br>
+              {{ getName(data.item, 'level') }}<br />
+              {{ getName(data.item, 'course') }}<br />
               <!-- {{ data.item.enrolledYear ? `Enrolled Year: ${data.item.enrolledYear}` : '' }} -->
             </span>
           </template>
-          <template v-slot:cell(studentCategory.name)="{ item: { studentCategory, studentCategoryId } }">
+          <template
+            v-slot:cell(studentCategory.name)="{
+              item: { studentCategory, studentCategoryId },
+            }"
+          >
             <b-badge
-              :variant="studentCategoryId === studentCategories.NEW.id
-                ? 'success'
-                : studentCategoryId === studentCategories.OLD.id ? 'primary' : 'warning'">
+              :variant="
+                studentCategoryId === studentCategories.NEW.id
+                  ? 'success'
+                  : studentCategoryId === studentCategories.OLD.id
+                  ? 'primary'
+                  : 'warning'
+              "
+            >
               {{ studentCategory.name }}
             </b-badge>
           </template>
           <template v-slot:cell(status)="data">
             <b-badge
-              :variant="data.item.evaluationStatusId === evaluationStatuses.APPROVED.id
-                ? 'success'
-                : data.item.evaluationStatusId === evaluationStatuses.REJECTED.id ? 'danger' : 'warning'">
-              {{ evaluationStatuses.getEnum(data.item.evaluationStatusId).name }}
+              :variant="
+                data.item.evaluationStatusId === evaluationStatuses.APPROVED.id
+                  ? 'success'
+                  : data.item.evaluationStatusId ===
+                    evaluationStatuses.REJECTED.id
+                  ? 'danger'
+                  : 'warning'
+              "
+            >
+              {{
+                evaluationStatuses.getEnum(data.item.evaluationStatusId).name
+              }}
             </b-badge>
           </template>
           <template v-slot:cell(action)="row">
-            <b-dropdown right variant="link" toggle-class="text-decoration-none" no-caret>
+            <b-dropdown
+              right
+              variant="link"
+              toggle-class="text-decoration-none"
+              no-caret
+            >
               <template v-slot:button-content>
                 <v-icon name="ellipsis-v" />
               </template>
               <b-dropdown-item
-                :to="`/master-files/student/${$route.params.studentId}/school-records/evaluations/${row.item.id}`">
+                :to="
+                  `/master-files/student/${$route.params.studentId}/school-records/evaluations/${row.item.id}`
+                "
+              >
                 Edit
               </b-dropdown-item>
             </b-dropdown>
@@ -88,24 +134,24 @@
   </div>
 </template>
 <script>
-import { StudentApi, EvaluationApi } from '../../../mixins/api'
-import { EvaluationStatuses, StudentCategories } from '../../../helpers/enum'
-import EvaluationEntry from './EvaluationEntry'
-import { copyValue } from '../../../helpers/extractor'
-import { clearFields, showNotification } from '../../../helpers/forms'
+import { StudentApi, EvaluationApi } from '../../../mixins/api';
+import { EvaluationStatuses, StudentCategories } from '../../../helpers/enum';
+import EvaluationEntry from './EvaluationEntry';
+import { copyValue } from '../../../helpers/extractor';
+import { clearFields, showNotification } from '../../../helpers/forms';
 
 export default {
-  mixins: [ StudentApi, EvaluationApi ],
+  mixins: [StudentApi, EvaluationApi],
   components: {
-    EvaluationEntry
+    EvaluationEntry,
   },
   created() {
     if (this.$route.query?.success) {
-      showNotification(this, "success", "Updated Successfully.")
-      this.$router.replace({'query': null});
+      showNotification(this, 'success', 'Updated Successfully.');
+      this.$router.replace({ query: null });
     }
-    this.studentId = this.$route.params.studentId
-    this.loadStudentEvaluationList()
+    this.studentId = this.$route.params.studentId;
+    this.loadStudentEvaluationList();
   },
   data() {
     return {
@@ -119,92 +165,95 @@ export default {
         evaluations: {
           isBusy: false,
           fields: [
-						{
-							key: "name",
-							label: "Name",
-							tdClass: "align-middle",
-							thStyle: { width: "auto"},
-							formatter: (value, key, item) => {
-								if(!item.student.middleName){
-									item.student.middleName = ""
-								}
-								item.student.name = item.student.firstName + " " + item.student.middleName + " " + item.student.lastName
-							}
-						},
-						{
-							key: "contact",
-							label: "Contact Info",
-							tdClass: "align-middle",
-							thStyle: { width: "20%" },
-
-						},
-						{
-							key: "education",
-							label: "Education",
-							tdClass: "align-middle",
-              thStyle: { width: "20%"}
+            {
+              key: 'name',
+              label: 'Name',
+              tdClass: 'align-middle',
+              thStyle: { width: 'auto' },
+              formatter: (value, key, item) => {
+                if (!item.student.middleName) {
+                  item.student.middleName = '';
+                }
+                item.student.name =
+                  item.student.firstName +
+                  ' ' +
+                  item.student.middleName +
+                  ' ' +
+                  item.student.lastName;
+              },
             },
             {
-              key: "studentCategory.name",
-							label: "Student Category",
-							tdClass: "align-middle text-center font-weight-bold",
-							thClass: "text-center",
-							thStyle: { width: "12%"}
-            },
-						{
-							key: "status",
-							label: "Status",
-							tdClass: "align-middle text-center",
-							thClass: "text-center",
-							thStyle: { width: "12%"}
+              key: 'contact',
+              label: 'Contact Info',
+              tdClass: 'align-middle',
+              thStyle: { width: '20%' },
             },
             {
-							key: "attachments",
-							label: "",
-							tdClass: "align-middle text-center",
-							thClass: "text-center",
-							thStyle: { width: "8%"}
-						},
-						{
-							key: "action",
-							label: "",
-							tdClass: "align-middle",
-							thStyle: { width: "40px"}
-						},
-					],
-          items: []
-        }
-      }
-    }
+              key: 'education',
+              label: 'Education',
+              tdClass: 'align-middle',
+              thStyle: { width: '20%' },
+            },
+            {
+              key: 'studentCategory.name',
+              label: 'Student Category',
+              tdClass: 'align-middle text-center font-weight-bold',
+              thClass: 'text-center',
+              thStyle: { width: '12%' },
+            },
+            {
+              key: 'status',
+              label: 'Status',
+              tdClass: 'align-middle text-center',
+              thClass: 'text-center',
+              thStyle: { width: '12%' },
+            },
+            // {
+            // 	key: "attachments",
+            // 	label: "",
+            // 	tdClass: "align-middle text-center",
+            // 	thClass: "text-center",
+            // 	thStyle: { width: "8%"}
+            // },
+            {
+              key: 'action',
+              label: '',
+              tdClass: 'align-middle',
+              thStyle: { width: '40px' },
+            },
+          ],
+          items: [],
+        },
+      },
+    };
   },
   methods: {
     loadStudentEvaluationList() {
-      const evaluationStatusId = EvaluationStatuses.APPROVED.id
-      const params = { paginate: false, evaluationStatusId }
-      const { evaluations } = this.tables
-      evaluations.isBusy = true
-      this.getEvaluationsOfStudent(this.studentId, params)
-      .then(({ data }) => {
-        evaluations.isBusy = false
-        evaluations.items = data
-      })
+      const evaluationStatusId = EvaluationStatuses.APPROVED.id;
+      const params = { paginate: false, evaluationStatusId };
+      const { evaluations } = this.tables;
+      evaluations.isBusy = true;
+      this.getEvaluationsOfStudent(this.studentId, params).then(({ data }) => {
+        evaluations.isBusy = false;
+        evaluations.items = data;
+      });
     },
-    avatar(student){
-      let src = ''
+    avatar(student) {
+      let src = '';
       if (student.photo) {
-        src = process.env.VUE_APP_PUBLIC_PHOTO_URL + student.photo.hashName
+        src = process.env.VUE_APP_PUBLIC_PHOTO_URL + student.photo.hashName;
       }
-      return src
+      return src;
     },
-    getName(item, child){
+    getName(item, child) {
       if (item) {
-        let value = item[child]
+        let value = item[child];
         if (value) {
-          return value['name']
+          return value['name'];
         }
       }
-      return ''
+      return '';
     },
-  }
-}
+  },
+};
 </script>
