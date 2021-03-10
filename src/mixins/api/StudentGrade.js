b94ffe26cb744c5e24cc82d1c6c800d67be1bf07
Jason Patawaran <patawaranjason17@gmail.com>
2021-03-10 22:00:32 +0800

    student grade api

---
+++ src/mixins/api/StudentGrade.js
@@ -0,0 +1,24 @@
+import { apiVersion, apiPrefix } from './config';
+
+export default {
+  methods: {
+    getStudentGradeList(params) {
+      return this.$authHttp.get(`${apiPrefix}/${apiVersion}/student-grades`, { params: params });
+    },
+    getStudentGrade(id) {
+      return this.$authHttp.get(`${apiPrefix}/${apiVersion}/student-grades/${id}`);
+    },
+    addStudentGrade(data) {
+      return this.$authHttp.post(`${apiPrefix}/${apiVersion}/student-grades`, data);
+    },
+    updateStudentGrade(data, id) {
+      return this.$authHttp.put(`${apiPrefix}/${apiVersion}/student-grades/${id}`, data);
+    },
+    batchUpdateStudentGrade(data) {
+      return this.$authHttp.post(`${apiPrefix}/${apiVersion}/student-grades/batch-update`, data);
+    },
+    deleteStudentGrade(id) {
+      return this.$authHttp.delete(`${apiPrefix}/${apiVersion}/student-grades/${id}`);
+    }
+  }
+}
