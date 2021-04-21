bc59ad90aedf762408b81300a6f9b9a653a78d25
Paul Christian Rueda <nexuslifeline@gmail.com>
2021-04-21 21:34:44 +0800

    updated academic transcript list

---
+++ src/views/components/ColumnDetails/CurriculumColumn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      {{ curriculumName }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      data: {
+        type: [Object]
+      }
+    },
+    computed: {
+      curriculumName() {
+        const { curriculum } = this.data;
+        return curriculum?.name || curriculum?.description || curriculum?.notes || 'No Curriculum';
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  @import "../../../assets/scss/shared.scss";
+</style>
--- src/views/components/ColumnDetails/index.js
+++ src/views/components/ColumnDetails/index.js
@@ -2,3 +2,4 @@ export { default as StudentColumn } from './StudentColumn';
 export { default as EducationColumn } from './EducationColumn';
 export { default as ContactColumn } from './ContactColumn';
 export { default as AddressColumn } from './AddressColumn';
+export { default as CurriculumColumn } from './CurriculumColumn';
