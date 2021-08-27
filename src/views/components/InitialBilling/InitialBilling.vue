e041beafb2eedb69d8f2ddaca7352ac7b002e3b8
edprnl <pecsonerick.ep@gmail.com>
2021-08-27 15:32:39 +0800

    initial billing loading overlay
    initial billing loading overlay

--- src/views/components/InitialBilling/InitialBilling.vue
+++ src/views/components/InitialBilling/InitialBilling.vue
@@ -8,23 +8,25 @@
       If the amount is incorrect, you can override the amount here.
       Marking the initial billing as <b>Unpaid</b> will <b>cancel</b> all the payments posted to it.
     </CardNote>
-    <InputGroup>
-      <InputContainer>
-        <InputInline>
-          <Toggle @input="onMarkUnpaid" v-model="isUnpaid"/>
-          <span class="ml-2">Mark as Unpaid</span>
-        </InputInline>
-      </InputContainer>
-      <div class="initial-fee__container">
-        <label class="required label">Registration Fee</label>
-        <!-- <b-form-input v-model="forms.billing.fields.totalAmount"/> -->
-        <vue-autonumeric
-          v-model="forms.billing.fields.totalAmount"
-          class="form-control text-right"
-          :options="[{modifyValueOnWheel: false,emptyInputBehavior: 0,},]"
-        />
-      </div>
-    </InputGroup>
+    <b-overlay :show="isLoading" rounded="sm">
+      <InputGroup>
+        <InputContainer>
+          <InputInline>
+            <Toggle @input="onMarkUnpaid" v-model="isUnpaid"/>
+            <span class="ml-2">Mark as Unpaid</span>
+          </InputInline>
+        </InputContainer>
+        <div class="initial-fee__container">
+          <label class="required label">Registration Fee</label>
+          <!-- <b-form-input v-model="forms.billing.fields.totalAmount"/> -->
+          <vue-autonumeric
+            v-model="forms.billing.fields.totalAmount"
+            class="form-control text-right"
+            :options="[{modifyValueOnWheel: false,emptyInputBehavior: 0,},]"
+          />
+        </div>
+      </InputGroup>
+    </b-overlay>
     <template v-slot:footer>
       <CardFooterRow>
         <b-button v-if="!isReadOnly" variant="primary" @click="onUpdateAmount" :disabled="isProcessing">
@@ -80,15 +82,18 @@ import { AcademicRecordApi, BillingApi } from '../../../mixins/api'
             states: { ...billingFields },
             errors: { ...billingFields }
           }
-        }
+        },
+        isLoading: true
       }
     },
     created() {
       // GET /academic-records/{academicRecordId}/initial-billing
       const { academicRecordId } = this
       const { fields } = this.forms.billing
+      this.isLoading = true
       this.getInitialBilling(academicRecordId).then(({ data }) => {
         copyValue(data, fields)
+        this.isLoading = false
       })
     },
     methods: {
