0ad9a70136b75cf8f3b41e99228a0ab0e0f2ffaa
Erick Pecson <pecsonerick.ep@gmail.com>
2021-11-23 20:59:58 +0800

    breadcrumb component
    breadcrumb component

---
+++ src/views/components/Breadcrumb/Breadcrumb.vue
@@ -0,0 +1,65 @@
+<template>
+  <ul class="breadcrum-container">
+    <li class="crumb" v-for="(item, index) in items" :key="index" @click="onItemClicked(item, index)" >
+      {{ item.text }}
+      <v-icon class="right-icon" name="chevron-right" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: [Array],
+      default: []
+    }
+  },
+  methods: {
+    onItemClicked(item, index) {
+      if(index === this.items.length - 1) {
+        return
+      }
+
+      this.$router.push(item.path)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .breadcrum-container{
+    display: flex;
+    align-items: center;
+    list-style-type: none; /* Remove bullets */
+    padding: 0; /* Remove padding */
+    margin: 0; /* Remove margins */
+  }
+
+  ul > li {
+    display: flex;
+    align-items: center;
+    font-size: 12pt;
+  }
+
+  ul > li:not(:last-child):hover {
+    cursor: pointer;
+    text-decoration: underline;
+    color: lightblue;
+
+  }
+
+  .right-icon {
+    color: lightgray;
+    margin: 0 10px;
+  }
+
+  ul li:last-child {
+    color: lightgray;
+    .right-icon {
+      display: none;
+    }
+  }
+
+
+</style>
--- src/views/pages/DemoPage2.vue
+++ src/views/pages/DemoPage2.vue
@@ -1,16 +1,41 @@
 <template>
   <div>
     <Transcript :transcriptId="1" />
+    <Breadcrumb :items="breadcrumbs"/>
   </div>
 </template>
 <script>
 import Transcript from '../components/Transcript/Transcript';
+import Breadcrumb from '../components/Breadcrumb/Breadcrumb.vue'
 
 export default {
   components: {
-    Transcript
+    Transcript,
+    Breadcrumb
   },
   mixins: [],
+  data() {
+    return {
+      breadcrumbs: [
+        {
+          text: 'Location',
+          path: '/'
+        },
+        {
+          text: 'Midwest',
+          path: '/'
+        },
+        {
+          text: 'Indiana',
+          path: '/'
+        },
+        {
+          text: 'Indianapolis',
+          path: '/'
+        }
+      ]
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
